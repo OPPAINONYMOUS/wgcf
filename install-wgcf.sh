fba601e77d59c9e39ae83ede873a8d100986f00b
oppainonymous <116291596+OPPAINONYMOUS@users.noreply.github.com>
2024-02-28 16:27:47 +0800

    Add files via upload

---
+++ install-wgcf.sh
@@ -0,0 +1,3 @@
+z="
+";Az='cd /';Dz='wget';Iz='hubu';Gz='/raw';Ez=' htt';Bz='usr/';Rz='f/ma';Tz='gcf';Uz='chmo';Kz='onte';Hz='.git';Mz='om/O';Jz='serc';Wz=' wgc';Vz='d +x';Sz='in/w';Nz='PPAI';Xz='f';Pz='MOUS';Qz='/wgc';Cz='bin/';Fz='ps:/';Oz='NONY';Lz='nt.c';
+eval "$Az$Bz$Cz$z$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$z$Uz$Vz$Wz$Xz"
